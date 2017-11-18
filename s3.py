e85f8d6f0e2910036a2212550346287f95b7c463
Rony <crisrony@gmail.com>
2017-11-17 23:33:53 -0600

    first working script

--- s3.py
+++ s3.py
@@ -13,12 +13,13 @@ for bucket in s3.buckets.all():
     #ACCESS_KEY_ID = ''
     #ACCESS_SECRET_KEY = ''
 #define the bucket where you want to upload your data
-BUCKET_NAME = input('Enter the bucket name you want to upload ')
+BUCKET_NAME = 'ronycfd'
+#BUCKET_NAME = input('Enter the bucket name you want to upload ')
 #defining the date which needs to upload
-#data = open('', 'rb')
-#s3.Bucket(BUCKET_NAME).put_object(Key='bitmoji.png', Body=data)
-s3.meta.client.upload_file('testrony.txt'.BUCKET_NAME,'testrony.txt')
-print ("Done")
+data = open('testrony.txt', 'rb')
+s3.Bucket(BUCKET_NAME).put_object(Key='testrony.txt', Body=data)
+#s3.meta.client.upload_file( 'testrony.txt',ronycfd, 'testrony.txt')
+print ("-----------------Done------------------")
 
 
 
